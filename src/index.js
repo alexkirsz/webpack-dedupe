7be04bd98b22c3eb714597216031dc76d442122a
Morhaus <alexandre.kirszenberg@gmail.com>
2015-06-14 00:11:03 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+global._React = React;
+require('foo');
+
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var fs = require('fs');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: path.resolve('./src/index.js'),
+  output: {
+    filename: 'output.js',
+  },
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(/^react$/, require.resolve('react')),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        include: [path.resolve('./src'), fs.realpathSync(path.resolve('./node_modules/foo/src'))],
+      },
+    ],
+  },
+};
